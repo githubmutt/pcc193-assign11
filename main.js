8608accb9f795fce073d6355f8e1d8f9357c1d12
githubmutt <github.mutt@gmail.com>
2022-05-04 02:16:36 -0700

    rendered

--- main.js
+++ main.js
@@ -1,52 +1,52 @@
-window.onload = fetch2
+window.onload = fetcher
 
+/*
 
+  https://githubmutt.github.io/pcc193-assign11/
+*/
 
-function debug(text){
+function write(text){
 
 
     let stuff = "%c text"
     
-    let v = document.querySelector(".output").innerHTML += text
+    let v = document.querySelector(".output").innerHTML += text + " &nbsp;&nbsp;&nbsp;"
 
-    console.log( v )
+    console.log( text)
 
 
  }
 
-function fetch2()
+function fetcher()
 {
 
-    let url = "https://sctennisclub.org/membership/json.php"
+   
+   let url = "https://jsonplaceholder.typicode.com/users"
 
-    fetch( url  ,{method: 'no-cors'})
+    fetch( url  , {method: 'get'})
     .then(response => response.json())
-    .then(data => console.log(data));
+    .then( data => {
 
-}
-function fetcher(){
+        console.log( data )
+        document.querySelector(".output").innerHTML =  "<h2>"
 
-     debug("initializing")
-    fetch( "https://jsonplaceholder.typicode.com/users" ,{ method: 'get'}).then(
+        for(let v of data){
+            
+            write( v.name + " (" + v.username + ") " + v.email + "<br>" )
+            write("WEBSITE: " + v.website + "<br>")
+            write("ADDRESS: " + v.address.street + v.address.suite + " " + v.address.city + " " + v.address.zipcode + "<br>")
+            write("GEO: " + v.address.geo.lat + " " + v.address.geo.lng)
+            write("<br>")
+            write(v.company.name + "<br>" )
+            write(v.company.catchPhrase + "<br>")
+            write(v.company.bs + "<br>")
 
-        function(response){
-                console.log(response)
-                debug(response)
-            }
-
-    ).then(
-
-        function(response){
-            debug("error")
-            console.log(response)
-
+            write("<p>")
         }
-    )
 
-
-
-
-
+    }
+    
+    );
 
 }
 
